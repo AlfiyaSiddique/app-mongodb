f2e9500db06cd614b2c2084931b2948bf19e799f
Alfiya Siddique <86224794+AlfiyaSiddique@users.noreply.github.com>
2024-08-11 19:35:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 
 app.use(express.static(__dirname+"/public"));
 app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 app.set("view engine", "ejs")
 
 // Get index Page
